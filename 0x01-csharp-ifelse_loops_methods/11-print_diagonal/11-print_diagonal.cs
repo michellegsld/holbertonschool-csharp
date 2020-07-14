48d950d26ff39aa8985e2707089c5940397bcba1
Michelle Giraldo <michellegsld@gmail.com>
2020-07-14 15:19:28 -0400

    [Initial] Use for-loop to print needed spaces and slashes

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            int slash;
+            string space = "";
+
+            if (length > 0)
+            {
+                for (slash = 0; slash < length; slash++)
+                {
+                    Console.WriteLine("{0}\\", space);
+                    space += " ";
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
