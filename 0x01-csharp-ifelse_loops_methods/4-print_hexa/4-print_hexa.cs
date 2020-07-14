7f7aff1616ce2b826e9c8cc9dd0e40fd33d22cf2
Michelle Giraldo <michellegsld@gmail.com>
2020-07-14 14:05:16 -0400

    [Initial] Use x to print hexadecimal form in lowercase

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -6,7 +6,11 @@ namespace _4_print_hexa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int decNum;
+            for (decNum = 0; decNum < 99; decNum++)
+            {
+                Console.Write("{0} = 0x{0:x}\n", decNum);
+            }
         }
     }
 }
