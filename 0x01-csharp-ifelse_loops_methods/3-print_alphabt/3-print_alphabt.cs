c976e9e35a7d44c6d51681c8f94f38f8e2be9f4f
Michelle Giraldo <michellegsld@gmail.com>
2020-07-14 13:55:10 -0400

    [Initial] Replica of task 4, except add if statement to not print q or e

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            for (ch = 'a'; ch <= 'z'; ch++)
+            {
+                if (ch != 'e' && ch != 'q')
+                {
+                    Console.Write(ch);
+                }
+            }
+        }
+    }
+}
