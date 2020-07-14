c39ed460cc493174b1259a7ea4860a2e9e27523a
Michelle Giraldo <michellegsld@gmail.com>
2020-07-14 15:24:53 -0400

    [Initial] Create FizzBuzz program

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            for (num = 1; num <= 100; num++)
+            {
+                if (num % 15 == 0)
+                {
+                    Console.Write("FizzBuzz");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                else
+                {
+                    Console.Write(num);
+                }
+                if (num == 100)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
