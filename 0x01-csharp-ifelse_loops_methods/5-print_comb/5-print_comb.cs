d11d847c800ebe92a6868b8e7531614900a5bf1c
Michelle Giraldo <michellegsld@gmail.com>
2020-07-14 14:24:36 -0400

    [Initial] Use for-loop to go through numbers to print

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            for (num = 0; num < 100; num++)
+            {
+                if (num != 99)
+                {
+                    Console.Write($"{num}, ");
+                }
+                else
+                {
+                    Console.Write($"{num}\n");
+                }
+            }
+        }
+    }
+}
