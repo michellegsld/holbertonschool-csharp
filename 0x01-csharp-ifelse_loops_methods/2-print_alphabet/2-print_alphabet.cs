2480743148f60a570aa649720abd4a2a66a7ba0e
Michelle Giraldo <michellegsld@gmail.com>
2020-07-14 13:44:18 -0400

    [Initial] Create char called ch that would loop from a to z and be printed

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            for(ch = 'a'; ch <= 'z'; ch++)
+            Console.Write(ch);
+        }
+    }
+}
