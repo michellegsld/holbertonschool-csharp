eccbae8e66295ad40142806f465bb0922c348340
Michelle Giraldo <michellegsld@gmail.com>
2020-07-14 13:30:01 -0400

    [Initial] Create a variable called digit, then print information about last digit of number

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int digit = ((number * number) / number) % (10);
+
+        Console.Write($"The last digit of {number} is {digit} and is ");
+        if (digit > 5)
+        {
+            Console.WriteLine("greater than 5");
+        }
+        else if (digit == 0)
+        {
+            Console.WriteLine("0");
+        }
+        else
+        {
+            Console.WriteLine("less than 6 and not 0");
+        }
+    }
+}
