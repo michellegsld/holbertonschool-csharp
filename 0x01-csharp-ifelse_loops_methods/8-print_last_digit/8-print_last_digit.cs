c7edf6f8fbd1fadab9e00b2e61cde64f30193c23
Michelle Giraldo <michellegsld@gmail.com>
2020-07-14 15:04:00 -0400

    [Initital] Return the last digit of a number

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            return Math.Abs(number) % 10;
+        }
+    }
+}
