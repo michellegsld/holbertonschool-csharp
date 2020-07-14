91fa3963aee71eaf79e2b441ece14287307eab97
Michelle Giraldo <michellegsld@gmail.com>
2020-07-14 14:50:55 -0400

    [Initial] Add nested for-loops that every combo of unique numbers

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ten, one, num;
+            for (ten = 0; ten < 9; ten++)
+            {
+                for (one = ten + 1; one < 10; one++)
+                {
+                    num = (ten * 10) + one;
+                    if (num != 89)
+                    {
+                        Console.Write("{0:00}, ", num);
+                    }
+                    else
+                    {
+                        Console.Write($"{num}\n");
+                    }
+                }
+            }
+        }
+    }
+}
