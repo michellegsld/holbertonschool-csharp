61ff334e44c54bf9d3b0c7b8cf52f5c4473b575d
Michelle Giraldo <michellegsld@gmail.com>
2020-07-16 18:29:00 -0400

    [Initial] Use for each loop in order to get total

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int total = 0;
+
+        foreach(KeyValuePair<string, string> item in myDict)
+            total++;
+
+        return total;
+    }
+}
