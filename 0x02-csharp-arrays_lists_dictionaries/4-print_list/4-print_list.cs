4e9cae01bfb08a39f581b6e884373fc6ad470fc9
Michelle Giraldo <michellegsld@gmail.com>
2020-07-16 17:20:23 -0400

    [Initial] Create a list and then use Insert

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> new_list;
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+
+        for (int i = 0; i < size; i++)
+            new_list.Insert(i, i);
+
+        return new_list;
+    }
+}
