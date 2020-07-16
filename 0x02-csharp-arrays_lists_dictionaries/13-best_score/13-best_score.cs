9a9f8e1c6113579d1288c965bff809f0c2f5f362
Michelle Giraldo <michellegsld@gmail.com>
2020-07-16 19:39:31 -0400

    [Initial] Use a list again to find the highest score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        List<int> sortedVals = new List<int>();
+        int i = 0;
+
+        if (myList.Count <= 0)
+            return "None";
+
+        foreach (int val in myList.Values)
+            sortedVals.Add(val);
+
+        sortedVals.Sort();
+        sortedVals.Reverse();
+
+        foreach(KeyValuePair<string, int> outitem in myList)
+        {
+            foreach(KeyValuePair<string, int> item in myList)
+                if (sortedVals[i] == item.Value)
+                    return item.Key;
+            i++;
+        }
+
+        return "None";
+    }
+}
