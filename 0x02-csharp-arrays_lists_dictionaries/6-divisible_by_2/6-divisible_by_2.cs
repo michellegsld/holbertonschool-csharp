d1d53871314eeccffada6d740af66c2eab1d4d2e
Michelle Giraldo <michellegsld@gmail.com>
2020-07-16 18:01:18 -0400

    [Initial] Create a boolean list to add true and false to it

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> boolList = new List<bool>();
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] % 2 == 0)
+                boolList.Add(true);
+            else
+                boolList.Add(false);
+        }
+
+        return boolList;
+    }
+}
