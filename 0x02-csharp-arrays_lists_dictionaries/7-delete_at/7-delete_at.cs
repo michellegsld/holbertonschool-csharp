01792a2a30d7aaa0b4d9e79beada9b252d1ddb79
Michelle Giraldo <michellegsld@gmail.com>
2020-07-16 18:17:46 -0400

    [Initial] Use Remove instead of RemoveAt

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return (myList);
+        }
+
+        myList.Remove(myList[index]);
+
+        return myList;
+    }
+}
