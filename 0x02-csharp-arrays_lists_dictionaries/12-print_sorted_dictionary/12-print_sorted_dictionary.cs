f277251ecf8994ae44c5c8e4b9d85193d255d664
Michelle Giraldo <michellegsld@gmail.com>
2020-07-16 19:29:10 -0400

    [Initial] Create a list and then print based off the list

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<String> sortedKeys = new List<String>();
+        int i = 0;
+
+        foreach (string key in myDict.Keys)
+            sortedKeys.Add(key);
+
+        sortedKeys.Sort();
+
+        foreach(KeyValuePair<string, string> outitem in myDict)
+        {
+            foreach(KeyValuePair<string, string> item in myDict)
+                if (sortedKeys[i] == item.Key)
+                    Console.WriteLine(item.Key + ": " + item.Value);
+            i++;
+        }
+    }
+}
