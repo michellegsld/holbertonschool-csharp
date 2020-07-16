64bffed2832a3e807b8ef84723b0c7eb504da053
Michelle Giraldo <michellegsld@gmail.com>
2020-07-16 17:42:40 -0400

    [Initial] Return -1 if length is 0, else returns the max integer

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+            Console.WriteLine("List is empty");
+            /* return -1;  <- not needed cuz if length is 0
+               then would return -1 below anyway */
+
+        myList.Sort();
+
+        return myList[myList.Count - 1];
+    }
+}
