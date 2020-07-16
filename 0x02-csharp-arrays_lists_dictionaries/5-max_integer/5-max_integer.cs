cbc0b022c5bc1ba566bcfbd2ccb1332f3ee63dba
Michelle Giraldo <michellegsld@gmail.com>
2020-07-16 17:46:55 -0400

    [Undo] Re-did original as they tested for empty list which didn't work the way I thought it did

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -6,10 +6,12 @@ class List
     public static int MaxInteger(List<int> myList)
     {
         if (myList.Count == 0)
+        {
             Console.WriteLine("List is empty");
-            /* return -1;  <- not needed cuz if length is 0 then would return -1 below anyway */
+            return (-1);
+        }
 
-        myList.Sort();  /* Can be left here because list never will be null */
+        myList.Sort();
 
         return myList[myList.Count - 1];
     }
