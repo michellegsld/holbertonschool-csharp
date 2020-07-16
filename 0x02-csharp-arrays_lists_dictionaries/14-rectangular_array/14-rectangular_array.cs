2371a441bacae0ae0f10160115a78b8eea856aca
Michelle Giraldo <michellegsld@gmail.com>
2020-07-16 19:48:07 -0400

    [Initial] Instead of setting initially, use nested for-loops

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array2d = new int[5, 5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (i == 2 && j == 2)
+                    array2d[i, j] = 1;
+                else
+                    array2d[i, j] = 0;
+
+                Console.Write(array2d[i, j]);
+                if (j < 4)
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
