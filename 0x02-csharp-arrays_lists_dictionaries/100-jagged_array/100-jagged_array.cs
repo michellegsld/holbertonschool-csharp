0a4d4e3bd0d112fc711ed67b826d543147eac5ef
Michelle Giraldo <michellegsld@gmail.com>
2020-07-16 20:13:43 -0400

    [Initial] Create a jagged array to print

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] jagged = new int[][]
+        {
+            new int[4],
+            new int[7],
+            new int[2]
+        };
+
+        for (int i = 0; i < jagged.Length; i++)
+        {
+            for (int j = 0; j < jagged[i].Length; j++)
+            {
+                jagged[i][j] = j;
+
+                Console.Write(jagged[i][j]);
+                if (j < jagged[i].Length - 1)
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
