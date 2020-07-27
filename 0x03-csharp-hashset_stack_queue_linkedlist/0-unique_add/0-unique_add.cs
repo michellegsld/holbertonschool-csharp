a1efba5c4b439d08ddd802421873f6fdc84e34a1
Michelle Giraldo <michellegsld@gmail.com>
2020-07-27 18:18:05 -0400

    [Initial] Push first file to test if HashSet worked

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        var hashSet = new HashSet<int>(myList);
+        int total = 0;
+
+        foreach (int element in hashSet)
+            total += element;
+
+        return total;
+    }
+}
