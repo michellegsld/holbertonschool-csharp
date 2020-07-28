73141625bcef4d4ffcd2322654f7c29c3608a365
Michelle Giraldo <michellegsld@gmail.com>
2020-07-27 23:35:42 -0400

    [Initial] Go through each node to find the node in which can add the number

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        var currentNode = myLList.First;
+        for (; currentNode != null; currentNode = currentNode.Next)
+        {
+            if (n < currentNode.Value)
+            {
+                myLList.AddBefore(currentNode, n);
+                break;
+            }
+        }
+        return currentNode;
+    }
+}
