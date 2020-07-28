c32ee2464d46a1d446d8f9f773ad8a13adb24b4e
Michelle Giraldo <michellegsld@gmail.com>
2020-07-27 22:06:17 -0400

    [Initial] Similar to stack but use queue instead

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+
+        if (aQueue.Contains(search))
+        {
+            List<String> removeElements = new List<String>();
+
+            foreach (String element in aQueue)
+            {
+                removeElements.Add(element);
+                if (element == search)
+                    break;
+            }
+            foreach (String element in removeElements)
+                aQueue.Dequeue();
+        }
+
+        aQueue.Enqueue(newItem);
+
+        return aQueue;
+    }
+}
