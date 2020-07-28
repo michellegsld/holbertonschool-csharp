8af854c50d5fa53346cbe1b01972234fec1a3581
Michelle Giraldo <michellegsld@gmail.com>
2020-07-27 23:42:52 -0400

    [Initial] Remove a node at a specific index depending on its place in the linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index >= 0 && index < myLList.Count)
+            myLList.Remove(index);
+    }
+}
