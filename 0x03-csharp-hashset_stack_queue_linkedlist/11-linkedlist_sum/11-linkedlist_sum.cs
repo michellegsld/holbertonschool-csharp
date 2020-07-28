d13e5d4f810e619976b35ee296f00ac1e4fa1a81
Michelle Giraldo <michellegsld@gmail.com>
2020-07-27 23:23:06 -0400

    [Initial] Add each element of a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int total = 0;
+
+        foreach (int element in myLList)
+            total += element;
+
+        return total;
+    }
+}
