3943066a89d615acd31c1cdb9c3f5ac43b8f1fac
Michelle Giraldo <michellegsld@gmail.com>
2020-07-27 23:40:22 -0400

    [Update] Handle if request to add node to end of linked list:

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -6,14 +6,19 @@ class LList
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
         var currentNode = myLList.First;
+
         for (; currentNode != null; currentNode = currentNode.Next)
         {
             if (n < currentNode.Value)
             {
                 myLList.AddBefore(currentNode, n);
-                break;
+                return currentNode;
             }
         }
-        return currentNode;
+
+        if (currentNode == null)
+            myLList.AddLast(n);
+
+        return myLList.Last;
     }
 }
