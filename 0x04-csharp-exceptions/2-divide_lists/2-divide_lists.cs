10eaf2c1eed9d1a62a6e7eb2b959bf9e29fb5a2f
Michelle Giraldo <michellegsld@gmail.com>
2020-07-28 17:44:24 -0400

    [Initial] Create for loop with a try/catch/catch under it

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> totals = new List<int>();
+        int i = 0;
+
+        for (; i < listLength; i++)
+        {
+            try
+             {
+                totals.Add(list1[i] / list2[i]);
+             }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                totals.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+        return(totals);
+    }
+}
