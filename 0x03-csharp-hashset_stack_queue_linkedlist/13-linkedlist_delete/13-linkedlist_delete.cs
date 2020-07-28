d75e8bd859d533c391632e6c5418869a03b30ee3
Michelle Giraldo <michellegsld@gmail.com>
2020-07-27 23:48:54 -0400

    [Update] Use actual node instead of index to delete

--- 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -5,7 +5,19 @@ class LList
 {
     public static void Delete(LinkedList<int> myLList, int index)
     {
-        if (index >= 0 && index < myLList.Count)
-            myLList.Remove(index);
+        var node = myLList.First;
+        int i = 0;
+
+        if (index < 0 || index >= myLList.Count)
+            return;
+
+        while (i < index && node != null)
+        {
+            i++;
+            node = node.Next;
+        }
+
+        if (node != null)
+            myLList.Remove(node);
     }
 }
