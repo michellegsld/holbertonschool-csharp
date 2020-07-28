d89c401f559f3cd9f36bdc6d24f4116585b5fd61
Michelle Giraldo <michellegsld@gmail.com>
2020-07-28 16:49:41 -0400

    [Initial] Use try/catch/finally to print about division

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int total = 0;
+        try
+        {
+            total = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            total = 0;
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {total}");
+        }
+    }
+}
