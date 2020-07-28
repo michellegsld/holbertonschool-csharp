b4697e87a2fcea069001eccb8f3f867320490e95
Michelle Giraldo <michellegsld@gmail.com>
2020-07-27 21:08:26 -0400

    [Initial] Output information on the Stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+
+        Console.WriteLine("Stack contains {0}: {1}", search, aStack.Contains(search));
+
+        if (aStack.Contains(search))
+        {
+            List<String> removeElements = new List<String>();
+
+            foreach (String element in aStack)
+            {
+                removeElements.Add(element);
+                if (element == search)
+                    break;
+            }
+            foreach (String element in removeElements)
+                aStack.Pop();
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
