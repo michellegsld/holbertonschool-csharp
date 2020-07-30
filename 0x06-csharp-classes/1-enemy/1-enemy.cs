6bf70d1558019852f35e333709c3b1a5663941e7
Michelle Giraldo <michellegsld@gmail.com>
2020-07-30 17:24:20 -0400

    [Initial] Create public field health

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// An class that defines a zombie.
+    /// Public fields: health.
+    /// </summary>
+    class Zombie
+    {
+        int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
