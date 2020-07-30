8958342584da6786f5277ca954b53ad6af578854
Michelle Giraldo <michellegsld@gmail.com>
2020-07-30 17:33:00 -0400

    [Fix] Add comments

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -4,12 +4,17 @@ namespace Enemies
 {
     /// <summary>
     /// An class that defines a zombie.
-    /// Public fields: health.
     /// </summary>
     class Zombie
     {
+        /// <summary>
+        /// A public field that is an int with no value.
+        /// </summary>
         int health;
 
+        /// <summary>
+        /// A public constructor that sets the value of health to 0.
+        /// </summary>
         public Zombie() => health = 0;
     }
 }
