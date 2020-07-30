0197f3a8492a6ed51bf362a5ffe51278e0327418
Michelle Giraldo <michellegsld@gmail.com>
2020-07-30 18:20:57 -0400

    [Initial] Create a private field called name with it's own public property

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enemies
+{
+    /// <summary>
+    /// An class that defines a zombie.
+    /// </summary>
+    class Zombie
+    {
+        /// <summary>
+        /// A private field that is an int with no value.
+        /// </summary>
+        private int health;
+
+        /// <summary>
+        /// A private field that is a string with a default value.
+        /// </summary>
+        private string name = "(No name)";
+
+        /// <summary>
+        /// A public constructor that sets the value of health to 0.
+        /// </summary>
+        public Zombie() => health = 0;
+
+        /// <summary>
+        /// A public property with a get and set.
+        /// </summary>
+        /// <value> What to set name to. </value>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        /// <summary>
+        /// A public constructor that throws an error if value less than 0.
+        /// </summary>
+        /// <param name="value">Must be greater than or equal to zero.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+
+        /// <summary>
+        /// A public method that just returns a value.
+        /// </summary>
+        /// <returns> The value of health of the Zombie object</returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
