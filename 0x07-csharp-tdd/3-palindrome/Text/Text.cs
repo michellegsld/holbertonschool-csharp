d50d9e4f83b629c01ab59fdc92232c97909016dd
Michelle Giraldo <michellegsld@gmail.com>
2020-08-04 12:54:07 -0400

    [Initial] Add documentation and check if a string is a palindrome

---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// A public class with one public method.
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// A public method that checks if a string is a palindrome.
+        /// </summary>
+        /// <param name="s">the string to check.</param>
+        /// <returns>True if a palindrome or False if not.</returns>
+        public static bool IsPalindrome(string s)
+        {
+            if (s.Length == 0)
+                return (True);
+
+            var stringLetters = new String((s.ToLower()).Where(Char.IsLetter).ToArray());
+
+            if (stringLetters == stringLetters.Reverse())
+                return (True);
+
+            return (False);
+        }
+    }
+}
