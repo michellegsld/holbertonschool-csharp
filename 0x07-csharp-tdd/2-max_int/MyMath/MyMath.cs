9d9ce1c315095491fa98dca1c3b4ffa992ec88c9
Michelle Giraldo <michellegsld@gmail.com>
2020-08-04 12:23:10 -0400

    [Initial] Add documentation and a test to find largest integer

---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// A public class with one public method.
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// A public method that returns the max integer in a list of integers.
+        /// </summary>
+        /// <param name="nums">The list of integers.</param>
+        /// <returns>The max integer in the list.</returns>
+        public static int Max(List<int> nums)
+        {
+            int maxInt;
+
+            if (list.Count == 0)
+                return (0);
+            else
+                maxInt = nums[0];
+
+            foreach (int num in nums)
+                if (num > maxInt)
+                    maxInt = num;
+
+            return (maxInt);
+        }
+    }
+}
