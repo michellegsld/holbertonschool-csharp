2967e8b743f295af20ec5a6a9f6b9bf16bcdeac5
Michelle Giraldo <michellegsld@gmail.com>
2020-08-04 12:58:57 -0400

    [Initial] Currently runs no real tests, just a set up to see if it is correct

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    [TextClass]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestMethod]
+        public void Test_AddTwoPositiveNumbers()
+        {
+            Assert.Pass();
+        }
+    }
+}
