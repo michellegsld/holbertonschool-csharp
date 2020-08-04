025006f667981f8baf70cafc5fc0aa4207906b60
Michelle Giraldo <michellegsld@gmail.com>
2020-08-04 00:17:06 -0400

    [Initial] Add documentation and create method

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// A class called Operations holding one method
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// A method that adds 2 numbers
+        /// </summary>
+        /// <param name="a">The first number</param>
+        /// <param name="b">The second number</param>
+        /// <returns>The sum of a + b</returns>
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
