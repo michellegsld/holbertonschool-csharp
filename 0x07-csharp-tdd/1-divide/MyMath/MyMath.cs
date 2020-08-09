dfb2127ec7a058e2adafccbdd0214492085b80ca
Michelle Giraldo <michellegsld@gmail.com>
2020-08-09 14:16:57 -0400

    [Fix] Add missing {}

--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -15,14 +15,16 @@ namespace MyMath
         /// <returns>A new matrix containing the divided elements or null.</returns>
         public static int[,] Divide(int[,] matrix, int num)
         {
-            int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
-
             if (matrix == null)
                 return (null);
 
             if (num == 0)
+            {
                 Console.WriteLine("Num cannot be 0");
                 return (null);
+            }
+
+            int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
 
             for (int i = 0; i < matrix.GetLength(0); i++)
                 for (int j = 0; j < matrix.GetLength(1); j++)
