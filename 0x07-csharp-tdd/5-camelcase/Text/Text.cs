bc10d2c9da9c805ddd8859042535e7f0d7dd8e44
Michelle Giraldo <michellegsld@gmail.com>
2020-08-09 14:26:08 -0400

    [Initial] Add method that counts how many words are in a sentence

---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// A public class containing one public method.
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// A method that determines how many words are in a string.
+        /// </summary>
+        /// <param name="s">The string.</param>
+        /// <returns>Number of words in s.</returns>
+        public static int CamelCase(string s)
+        {
+            int total = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i == 0 || Char.IsUpper(s[i]))
+                    total++;
+            }
+
+            return (total);
+        }
+    }
+}
