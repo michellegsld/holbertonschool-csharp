b6d36761a5145c7037e2c6dd69808adc39619312
Michelle Giraldo <michellegsld@gmail.com>
2020-08-09 14:24:02 -0400

    [Initial] Find the index of the first unique character

---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// A public class with one public method.
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// A public method that returns the first non-repeating character's index.
+        /// </summary>
+        /// <param name="s">The string to find the character in.</param>
+        /// <returns>The index of the first non-repeating character or -1.</returns>
+        public static int UniqueChar(string s)
+        {
+            // Wouldn't go into this for-loop if length of string was 0
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        if (s[i] == s[j])
+                        {
+                            break;
+                        }
+                        else if (j == s.Length - 1 && s[i] != s[j])
+                        {
+                            return (i);
+                        }
+                    }
+                    else if (i == s.Length - 1)
+                    {
+                        return (i);
+                    }
+                }
+            }
+
+            // Either no unique char was found or string was empty
+            return (-1);
+        }
+    }
+}
