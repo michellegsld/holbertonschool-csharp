ac9836f204d8db71fa9d2f9c8928704c436e44d2
Michelle Giraldo <michellegsld@gmail.com>
2020-08-09 23:49:39 -0400

    [Initial] Create new derived class and add private fields as well as corresponding public properties

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// A base class with one public method.
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Throws an exception error with a message.
+    /// </summary>
+    /// <returns>An exception.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// A class that inherits from Shape with two private fields and two public properties.
+/// </summary>
+class Rectangle : Shape
+{
+    /// <summary>
+    /// Private fields.
+    /// </summary>
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// A public property with a get and set.
+    /// </summary>
+    /// <value> What to set width to.</value>
+    public int Width
+    {
+        get => width;
+        set {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
+        }
+    }
+
+    /// <summary>
+    /// A public property with a get and set.
+    /// </summary>
+    /// <value> What to set height to.</value>
+    public int Height
+    {
+        get => height;
+        set {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
+        }
+    }
+}
