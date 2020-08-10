cd1188f9cc361091915e9b143a5ca04c1099c567
Michelle Giraldo <michellegsld@gmail.com>
2020-08-09 21:16:45 -0400

    [Initial] Use if statement to check type of object with .GetType() and typeof()

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+            return (true);
+        return (false);
+    }
+}
