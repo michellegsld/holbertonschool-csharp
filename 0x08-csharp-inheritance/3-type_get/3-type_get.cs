89ac0070605040082adbd22885a16b5dd0f3a27f
Michelle Giraldo <michellegsld@gmail.com>
2020-08-09 23:02:08 -0400

    [Initial] Use two for-loops in order to print information needed:

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary>
+/// A class containing one public static void method.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// A method that prints the names of the available properties and methods of an object.
+    /// </summary>
+    /// <param name="myObj">The object to print info from.</param>
+    public static void Print(object myObj)
+    {
+        // NOTE: var is not specific! This is what var is replacing in these cases:
+        // PropertyInfo
+        // MethodInfo
+        // So instead of var, you can place these to be more specific
+
+        Console.WriteLine("{0} Properties:", myObj.GetType().Name);
+        foreach(var item in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine(item.Name);
+        }
+
+        Console.WriteLine("{0} Methods:", myObj.GetType().Name);
+        foreach(var item in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
