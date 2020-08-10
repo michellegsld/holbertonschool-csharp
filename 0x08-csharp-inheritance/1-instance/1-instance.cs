9fbb737dbfd9deb0812f5f96b69c4c5f27d43304
Michelle Giraldo <michellegsld@gmail.com>
2020-08-09 23:05:12 -0400

    [Fix] Add comments

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
+/// <summary>
+/// A class containing one public static bool method.
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// A method that returns true if an object is an instance of another class.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>true or false.</returns>
     public static bool IsInstanceOfArray(object obj)
     {
         return typeof(Array).IsInstanceOfType(obj);
