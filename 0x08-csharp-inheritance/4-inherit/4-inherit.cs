1fd170536a52193cbd28306468fadfc721e807dc
Michelle Giraldo <michellegsld@gmail.com>
2020-08-09 23:30:32 -0400

    [Initial] Create two classes, one of which is a derived class

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// An empty base class.
+/// </summary>
+class Animal
+{}
+
+/// <summary>
+/// An empty class inheriting from the empty class called Animal.
+/// </summary>
+class Dog : Animal
+{}
