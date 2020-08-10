5e17e76f4996f7953bfbc39406b61be029b08a83
Michelle Giraldo <michellegsld@gmail.com>
2020-08-09 23:06:16 -0400

    [Fix] Add comments

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
+/// <summary>
+/// A class containing one public static bool method.
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// A method that returns true if an object is an int, else false.
+    /// </summary>
+    /// <param name="obj">The object to check if is an int.</param>
+    /// <returns>true or false.</returns>
     public static bool IsOfTypeInt(object obj)
     {
         return (obj.GetType() == typeof(int));
