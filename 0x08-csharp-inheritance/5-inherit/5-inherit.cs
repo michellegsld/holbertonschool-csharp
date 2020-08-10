f6f86098298a78a8544723db4985d951e1b442d3
Michelle Giraldo <michellegsld@gmail.com>
2020-08-09 23:33:25 -0400

    [Initial] Throw an exception under a class called Shape

---
+++ 0x08-csharp-inheritance/5-inherit/5-inherit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// A class with one public method.
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Throws an exception error with a message.
+    /// </summary>
+    /// <returns>An exception.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
