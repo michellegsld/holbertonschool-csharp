8e89710f02eae2e3e9066db33d8a6056e99b0902
Michelle Giraldo <michellegsld@gmail.com>
2020-08-19 10:36:20 -0400

    [Initial] Do it the slow way with getPixel, SetPixel

---
+++ image_processor/ImageProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+
+class ImageProcessor
+{
+    public static void Inverse(string[] filenames)
+    {
+        Parallel.ForEach(filenames, filename =>
+        {
+            int x = 0;
+            int y = 0;
+
+            string file = filename.Substring(7, filename.Length - 11);
+
+            Bitmap inverted = new Bitmap(filename);
+
+            for (; x < inverted.Width; x++)
+            {
+                for (y = 0; y < inverted.Height; y++)
+                {
+                    Color currentPixel = inverted.GetPixel(x, y);
+                    currentPixel = Color.FromArgb(255 - currentPixel.R, 255 - currentPixel.G, 255 - currentPixel.B);
+                    inverted.SetPixel(x, y, currentPixel);
+                }
+            }
+
+            inverted.Save(file + "_inverted.jpg");
+        }
+        );
+    }
+}
