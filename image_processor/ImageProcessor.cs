31b5d52a647acb8cebdaa89b3e62263d865699fc
Michelle Giraldo <michellegsld@gmail.com>
2020-08-19 11:49:57 -0400

    [End-Test] Re-put everything

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -45,4 +45,44 @@ class ImageProcessor
         }
         );
     }
+
+    /// <summary>
+    /// Public class to turn an image into grayscale
+    /// </summary>
+    /// <param name="filenames">List of image locations to invert</param>
+    public static void Grayscale(string[] filenames)
+    {
+        Parallel.ForEach(filenames, filename =>
+        {
+            string file = Path.GetFileNameWithoutExtension(filename);
+            string extension = Path.GetExtension(filename);
+
+            Bitmap grayscale = new Bitmap(filename);
+
+            Rectangle rect = new Rectangle(0, 0, grayscale.Width, grayscale.Height);
+            System.Drawing.Imaging.BitmapData grayscaleData =
+                grayscale.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                grayscale.PixelFormat);
+
+            IntPtr ptr = grayscaleData.Scan0;
+
+            int bytes = Math.Abs(grayscaleData.Stride) * grayscale.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            for (int i = 0; i < rgbValues.Length - 3; i += 3)
+            {
+                byte gray = (byte)(rgbValues[i] * .21 + rgbValues[i + 1] * .71 + rgbValues[i + 2] * .071);
+                rgbValues[i] = rgbValues[i + 1] = rgbValues[i + 2] = gray;
+            }
+
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            grayscale.UnlockBits(grayscaleData);
+
+            grayscale.Save(file + "_grayscale" + extension);
+        }
+        );
+    }
 }
