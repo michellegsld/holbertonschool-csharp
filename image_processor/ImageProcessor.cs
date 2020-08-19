3e1feaf5c6f0bc2eea06065fb54ece652965bc31
Michelle Giraldo <michellegsld@gmail.com>
2020-08-19 11:02:01 -0400

    [Update] Use faster way with LockBits instead of SetPixel/GetPixel

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -4,29 +4,42 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Class
+/// </summary>
 class ImageProcessor
 {
+    /// <summary>
+    /// Public class to invert an image
+    /// </summary>
+    /// <param name="filenames">List of image locations to invert</param>
     public static void Inverse(string[] filenames)
     {
         Parallel.ForEach(filenames, filename =>
         {
-            int x = 0;
-            int y = 0;
-
             string file = Path.GetFileNameWithoutExtension(filename);
             string extension = Path.GetExtension(filename);
 
             Bitmap inverted = new Bitmap(filename);
 
-            for (; x < inverted.Width; x++)
-            {
-                for (y = 0; y < inverted.Height; y++)
-                {
-                    Color currentPixel = inverted.GetPixel(x, y);
-                    currentPixel = Color.FromArgb(255 - currentPixel.R, 255 - currentPixel.G, 255 - currentPixel.B);
-                    inverted.SetPixel(x, y, currentPixel);
-                }
-            }
+            Rectangle rect = new Rectangle(0, 0, inverted.Width, inverted.Height);
+            System.Drawing.Imaging.BitmapData invertedData =
+             inverted.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+             inverted.PixelFormat);
+
+            IntPtr ptr = invertedData.Scan0;
+
+            int bytes = Math.Abs(invertedData.Stride) * inverted.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            for (int i = 0; i < rgbValues.Length; i++)
+                rgbValues[i] = (byte)(255 - rgbValues[i]);
+
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            inverted.UnlockBits(invertedData);
 
             inverted.Save(file + "_inverted" + extension);
         }
