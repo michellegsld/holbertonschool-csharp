f0b6d5c28f204adcd6fe6003026a0bc0bde14bb2
Michelle Giraldo <michellegsld@gmail.com>
2020-08-26 12:04:37 -0400

    [Initial] Create new matrix that is the sum of two, else returns -1

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] fail = { { -1 } };
+
+        if (matrix1.GetLength(0) != matrix1.GetLength(1) || matrix2.GetLength(0) != matrix2.GetLength(1))
+            return (fail);
+
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+            return (fail);
+
+        if (matrix1.GetLength(0) != 2 && matrix1.GetLength(0) != 3)
+            return (fail);
+
+        double[,] sum = null;
+
+        if (matrix1.GetLength(0) == 2)
+            sum = new double[2, 2];
+        else
+            sum = new double[3, 3];
+
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+                sum[i, j] = matrix1[i, j] + matrix2[i, j];
+
+        return (sum);
+    }
+}
