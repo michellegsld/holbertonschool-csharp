6a88cfef960e73961a960a1bff653d28e5bf09da
Michelle Giraldo <michellegsld@gmail.com>
2020-08-26 00:47:51 -0400

    [Initial] Return -1 if not a 2D or 3D vector, else return vector * scalar

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// A class containing a public method reguarding vector and scalars
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Multiplies a vector by a scalar
+    /// </summary>
+    /// <param name="vector">The vector to be multiplied</param>
+    /// <param name="scalar">The scalar to multiply the vector by</param>
+    /// <returns></returns>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] fail = {-1};
+
+        if (vector.GetLength(0) != 2 && vector.GetLength(0) != 3)
+            return (fail);
+
+        if (vector.GetLength(0) == 2)
+        {
+            double[] sum = {scalar * vector[0], scalar * vector[1]};
+            return (sum);
+        }
+        else
+        {
+            double[] sum = {scalar * vector[0], scalar * vector[1], scalar * vector[2]};
+            return (sum);
+        }
+    }
+}
