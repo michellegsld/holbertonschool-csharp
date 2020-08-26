e4369ec61e89a61903c7ee8eeae88b161e72593b
Michelle Giraldo <michellegsld@gmail.com>
2020-08-26 16:49:05 -0400

    [Initial] Instead of using loops, just do math directly

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/// <summary>
+/// A class reguarding matrices
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Calculates the determinant of a matrix
+    /// </summary>
+    /// <param name="matrix">The matrix to find the determinate of</param>
+    /// <returns>The determinant or -1</returns>
+    public static double Determinant(double[,] matrix)
+    {
+        if (matrix.GetLength(0) != matrix.GetLength(1))
+            return (-1);
+        if (matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3)
+            return (-1);
+
+        if (matrix.GetLength(0) == 2)
+            return (matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0]);
+
+        double a = matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[1, 2] * matrix[2, 1]);
+        double b = matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 0]);
+        double c = matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
+
+        return (a - b + c);
+    }
+}
