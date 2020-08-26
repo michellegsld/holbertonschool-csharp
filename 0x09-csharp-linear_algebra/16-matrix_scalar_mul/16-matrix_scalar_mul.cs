d94e8f070467e5f72a2a99ad8484f0a701fa36dd
Michelle Giraldo <michellegsld@gmail.com>
2020-08-26 12:31:03 -0400

    [Fix] Correct variable name from matrix1 to matrix

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -15,22 +15,22 @@ class MatrixMath
     {
         double[,] fail = { { -1 } };
 
-        if (matrix1.GetLength(0) != matrix1.GetLength(1))
+        if (matrix.GetLength(0) != matrix.GetLength(1))
             return (fail);
 
-        if (matrix1.GetLength(0) != 2 && matrix1.GetLength(0) != 3)
+        if (matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3)
             return (fail);
 
         double[,] total = null;
 
-        if (matrix1.GetLength(0) == 2)
+        if (matrix.GetLength(0) == 2)
             total = new double[2, 2];
         else
             total = new double[3, 3];
 
-        for (int i = 0; i < matrix1.GetLength(0); i++)
-            for (int j = 0; j < matrix1.GetLength(1); j++)
-                total[i, j] = scalar * matrix1[i, j];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                total[i, j] = scalar * matrix[i, j];
 
         return (total);
     }
