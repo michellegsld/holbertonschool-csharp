a6b88146d00f912efa3deadb67c0c264ffdd8bad
Michelle Giraldo <michellegsld@gmail.com>
2020-08-26 16:14:41 -0400

    [Initial] Create empty matrix and return it if matrix to be transposed can't be, else use for-loops to transpose

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// A class reguarding matrices
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// A public method that transposes a matrix
+    /// </summary>
+    /// <param name="matrix">The matrix to transpose</param>
+    /// <returns>The resulting matrix</returns>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] final = {};
+
+        if (matrix == null || matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+            return (final);
+
+        final = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                final[j, i] = matrix[i, j];
+
+        return (final);
+    }
+}
