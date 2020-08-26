7435d8f8724d7caa6b7762e28f3adbc73b323e97
Michelle Giraldo <michellegsld@gmail.com>
2020-08-26 19:47:47 -0400

    [Initial] Find inverse of 2D array

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace _33_inverse_2D
+/// <summary>
+/// A class reguarding matrices
+/// </summary>
+class MatrixMath
 {
-    class Program
+    /// <summary>
+    /// A public method that calculates the inverse of a 2D matrix
+    /// </summary>
+    /// <param name="matrix">The matrix to find the inverse of</param>
+    /// <returns>The inverse of the matrix</returns>
+    public static double[,] Inverse2D(double[,] matrix)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        double[,] fail = { { -1 } };
+
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return (fail);
+
+        double a = matrix[0, 0];
+        double b = -1 * matrix[0, 1];
+        double c = -1 * matrix[1, 0];
+        double d = matrix[1, 1];
+        double determinant = a * d - b * c;
+
+        double[,] inverse = new double[2, 2];
+
+        inverse[0, 0] = d / determinant;
+        inverse[0, 1] = b / determinant;
+        inverse[1, 0] = c / determinant;
+        inverse[1, 1] = a / determinant;
+
+        return (inverse);
     }
 }
