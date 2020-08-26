ffbc1611a62e7ddb0f73a81004a5b13d9a48694c
Michelle Giraldo <michellegsld@gmail.com>
2020-08-25 23:52:29 -0400

    [Initial] Use math to find length of 2D/3D vector and return it, else returns -1

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Math;
 
+/// <summary>
+/// A class containing a public method reguarding vectors
+/// </summary>
 class VectorMath
 {
+    /// <summary>
+    /// To find the magnitude or length of a 2D or 3D vector
+    /// </summary>
+    /// <param name="vector">The vector in which to find the length of</param>
+    /// <returns>The length of the vector, else -1 if not 2D or 3D</returns>
     public static double Magnitude(double[] vector)
     {
         if (vector.GetLength(0) != 2 && vector.GetLength(0) != 3)
@@ -11,10 +19,8 @@ class VectorMath
         int sum = 0;
 
         foreach (int num in vector)
-        {
             sum += Pow(num, 2.0);
-        }
 
-        return (Sqrt(sum));
+        return (Round(Sqrt(sum), 2));
     }
 }
