f8845ee3b14fc762b4670e64c5768ef7e0d01c18
Michelle Giraldo <michellegsld@gmail.com>
2020-08-25 23:50:02 -0400

    [Initial] Change version number and add documentation line

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Math;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.GetLength(0) != 2 && vector.GetLength(0) != 3)
+            return (-1);
+
+        int sum = 0;
+
+        foreach (int num in vector)
+        {
+            sum += Pow(num, 2.0);
+        }
+
+        return (Sqrt(sum));
+    }
+}
