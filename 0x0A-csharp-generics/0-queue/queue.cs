e032c9990cc6d73e50053caa9f2229f38d86b6f4
Michelle Giraldo <michellegsld@gmail.com>
2020-10-26 18:12:29 -0400

    [Initial] Returns the type of T

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// The generic Queue class.
+/// </summary>
+/// <typeparam name="T">The generic type parameter.</typeparam>
+class Queue<T>
+{
+    /// <summary>
+    /// Method that returns the Queue's type.
+    /// </summary>
+    /// <returns>The type of whatever T is.</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
