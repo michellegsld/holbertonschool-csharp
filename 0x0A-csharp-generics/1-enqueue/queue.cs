ed87c8f110a2b0b83e33e3ce3e48fd09619ec404
Michelle Giraldo <michellegsld@gmail.com>
2020-10-26 18:34:00 -0400

    [Initial] Build off previous, add Enqueue() and count

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+/// <summary>
+/// The generic Queue class.
+/// </summary>
+/// <typeparam name="T">The generic type parameter.</typeparam>
+class Queue<T>
+{
+    /// <summary>
+    /// Must be an object of type Node.
+    /// </summary>
+    public Node head = null;
+    /// <summary>
+    /// Must be an object of type Node.
+    /// </summary>
+    public Node tail = null;
+    /// <summary>
+    /// Keeps track of amount of nodes.
+    /// </summary>
+    int count = 0;
+
+    /// <summary>
+    /// Method that returns the Queue's type.
+    /// </summary>
+    /// <returns>The type of whatever T is.</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary>
+    /// A public class with a couple of properties.
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// Can be of any type, initially set to null.
+        /// </summary>
+        T value = null;
+        /// <summary>
+        /// Must be an object of type Node, initally set to null.
+        /// </summary>
+        public Node next = null;
+
+        /// <summary>
+        /// Constructor for new Node.
+        /// </summary>
+        /// <param name="val">What to set value to.</param>
+        public Node (T val)
+        {
+            value = val;
+        }
+    }
+
+    /// <summary>
+    /// Creates a new node and adds it to the end of queue.
+    /// </summary>
+    /// <param name="val">The value of the node.</param>
+    public void Enqueue(T val)
+    {
+        Node newNode = new Node(val);
+
+        if (head == null)
+        {
+            tail = head = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    /// <summary>
+    /// A public method.
+    /// </summary>
+    /// <returns>The number of nodes in the queue.</returns>
+    public int Count() {
+        return count;
+    }
+}
