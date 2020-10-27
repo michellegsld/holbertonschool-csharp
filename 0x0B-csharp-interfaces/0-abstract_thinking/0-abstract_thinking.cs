2c56e35ce40deb2510a41c3727b92830316395e6
Michelle Giraldo <michellegsld@gmail.com>
2020-10-27 17:26:05 -0400

    [Initial] Add comments and create name property as well as override ToString()

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// An abstract class, contains public property name and overrides ToString().
+/// </summary>
+public abstract class Base
+{
+    public string name;
+
+    /// <summary>
+    /// Overrides the ToString() method.
+    /// </summary>
+    /// <returns> A string with the set up: `name` is a `type` </returns>
+    public override String ToString()
+    {
+        return $"{name} is a {this.GetType()}";
+    }
+}
