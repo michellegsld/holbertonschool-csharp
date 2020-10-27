1d98c2219252e0f86e9b6ddd9a15fcbdb0b098f9
Michelle Giraldo <michellegsld@gmail.com>
2020-10-27 18:38:48 -0400

    [Initial] Create interfaces and class that inherits them as well as base class

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+/// <summary>
+/// An abstract class, contains public property name and overrides ToString().
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public property that should be a string.
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    /// Overrides the ToString() method.
+    /// </summary>
+    /// <returns> A string with the set up: `name` is a `type` </returns>
+    public override String ToString() {
+        return $"{name} is a {this.GetType()}";
+    }
+}
+
+/// <summary>
+/// An interface called IInteractive.
+/// </summary>
+interface IInteractive
+{
+    /// <summary>
+    /// A method that currently does nothing.
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// An interface called IBreakable.
+/// </summary>
+interface IBreakable
+{
+    /// <summary>
+    /// An int property.
+    /// </summary>
+    int durability {get; set;}
+
+    /// <summary>
+    /// A method that currently does nothing.
+    /// </summary>
+    void Break();
+}
+
+/// <summary>
+/// An interface called ICollectable.
+/// </summary>
+interface ICollectable
+{
+    /// <summary>
+    /// A boolean property.
+    /// </summary>
+    bool isCollected {get; set;}
+
+    /// <summary>
+    /// A method that currently does nothing.
+    /// </summary>
+    void Collect();
+}
+
+/// <summary>
+/// A new empty public class that inherits from Base and all interfaces.
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+
+}
