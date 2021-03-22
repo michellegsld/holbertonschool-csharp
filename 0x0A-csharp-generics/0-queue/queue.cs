df42e185fc4d158f387fdd3bad038cafb0bdcf89
Michelle Giraldo <michellegsld@gmail.com>
2021-03-22 16:06:41 -0400

    [Initial] Use typeof() in order to return the Queue's type

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// The generic Queue class.
+/// </summary>
+class Queue<T>
+{
+    /// <summary>
+    /// Method that returns the Queue's type.
+    /// </summary>
+    /// <returns>The type of whatever T is.</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
