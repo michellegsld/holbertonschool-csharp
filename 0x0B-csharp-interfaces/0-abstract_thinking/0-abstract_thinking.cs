a1cddf27d072763de86b7cc82ab2c7dd1e34a896
Michelle Giraldo <michellegsld@gmail.com>
2021-03-23 16:01:28 -0400

    [Initial] Create an abstract class and create a public property and override the ToString() method

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary> An abstract class, contains public property name and overrides ToString(). </summary>
+public abstract class Base
+{
+    ///<summary> Public property that should be a string. </summary>
+    public string name;
+
+    /// <summary> Overrides the ToString() method. </summary>
+    /// <returns> A string with the set up: `name` is a `type`. </returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType()}";
+    }
+}
