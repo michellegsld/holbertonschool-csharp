b62825222a1d2d37224198d5e63fc96f4a371fe3
Michelle Giraldo <michellegsld@gmail.com>
2021-03-23 18:12:27 -0400

    [Fix] Remove public from name property

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -4,7 +4,7 @@
 public abstract class Base
 {
     /// <summary> Public property that should be a string. </summary>
-    public string name { set; get; }
+    string name { set; get; }
 
     /// <summary> Overrides the ToString() method. </summary>
     /// <returns> A string with the set up: `name` is a `type`. </returns>
