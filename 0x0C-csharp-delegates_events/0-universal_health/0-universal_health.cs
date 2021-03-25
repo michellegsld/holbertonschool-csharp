457e74ae865788330acbbe53fd156f045081fe6a
Michelle Giraldo <michellegsld@gmail.com>
2021-03-25 15:31:28 -0400

    [Initial] Create a Player class

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary> A public class with the properties `name`, `maxHp`, `hp` and a constructor. </summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary> Creates the Player and sets the properties. </summary>
+    /// <param name="name"> The name of Player, default is set to `Player`. </param>
+    /// <param name="maxHp"> The max HP of Player, default is set to `100f`. </param>
+    public Player(string name = "Player", float maxHp = 100f) {
+        this.name = name;
+        if (maxHp < 1) {
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    /// <summary> Prints information regarding the Player's HP.</summary>
+    public void PrintHealth() {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
