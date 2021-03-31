8643bf923cf53091c4477803ec6d5c415bb20349
Michelle Giraldo <michellegsld@gmail.com>
2021-03-31 18:48:02 -0400

    [Initial] Just create base file with comments and properties to be edited

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    /// <summary> A public class that sets information regarding an item. </summary>
+    public class Item : BaseClass
+    {
+        /// <summary> The name of an item. </summary>
+        /// <value> A string. Required. </value>
+        public string name { get; set; }
+
+        /// <summary> A description of the item. </summary>
+        /// <value> A string. Optional. </value>
+        public string description { get; set; }
+
+        /// <summary> The price of the item. </summary>
+        /// <value> A float limited to 2 decimal points. Optional. </value>
+        public float price { get; set; }
+
+        /// <summary> The tags of the item. </summary>
+        /// <value> A list of strings. Optional. </value>
+        public List<string> tags { get; set; }
+    }
+}
