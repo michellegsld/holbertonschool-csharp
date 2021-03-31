644e01490cb15a46d8b58a9fa3af84d86fd0a969
Michelle Giraldo <michellegsld@gmail.com>
2021-03-31 18:30:10 -0400

    [Update] Create properties that BaseClass has to define

--- 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -2,7 +2,19 @@
 
 namespace InventoryLibrary
 {
+    /// <summary> The class all other classes will inherit from. </summary>
     public class BaseClass
     {
+        /// <summary> The id of an object. </summary>
+        /// <value> A string. </value>
+        public string id { get; set; }
+
+        /// <summary> The time an object was created. </summary>
+        /// <value> A DateTime. </value>
+        public DateTime date_created { get; set; }
+
+        /// <summary> The time an object was updated. </summary>
+        /// <value> A DateTime. </value>
+        public DateTime date_updated { get; set; }
     }
 }
