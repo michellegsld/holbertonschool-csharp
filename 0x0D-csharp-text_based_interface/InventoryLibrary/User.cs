f1786ca2b863d019d52500e7354b79d8edbc07e4
Michelle Giraldo <michellegsld@gmail.com>
2021-03-31 18:49:10 -0400

    [Initial] Just create base file with comments and property to be edited

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace InventoryLibrary
+{
+    /// <summary> A public class that sets information regarding the User. </summary>
+    public class User : BaseClass
+    {
+        /// <summary> The name of a User. </summary>
+        /// <value> A string. Required. </value>
+        public string name { get; set; }
+    }
+}
