ca580bafdc84819f7d3ef688c9b8e7003d91de7e
Michelle Giraldo <michellegsld@gmail.com>
2021-03-31 22:02:20 -0400

    [Update] Add comments and make sure to add all properties to be set

--- 0x0D-csharp-text_based_interface/InventoryLibrary/Inventory.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/Inventory.cs
@@ -14,8 +14,14 @@ namespace InventoryLibrary
         /// <summary> The amount of items in the inventory. </summary>
         public int quantity;
 
+        /// <summary> Sets the information on an Inventory object. </summary>
+        /// <param name="u_id"> The id of the User. </param>
+        /// <param name="i_id"> The id of the Item. </param>
+        /// <param name="total"> The amount of items in the inventory. </param>
         public Inventory(string u_id = this.User.id, string i_id = this.Item_id.id, int total = 1)
         {
+            this.user_id = u_id;
+            this.item_id = i_id;
             if (total < 1)
                 total = 1;
             this.quantity = total;
