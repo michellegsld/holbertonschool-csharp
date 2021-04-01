36bd48c8662963b2ae9ad83091930b4d3636332e
Michelle Giraldo <michellegsld@gmail.com>
2021-04-01 12:34:10 -0400

    [Update] Fix errors, change Save() and Load() methods, figure out dictonary types

--- 0x0D-csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
@@ -1,9 +1,10 @@
 using System;
 using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Collections.Generic;
 
 namespace InventoryLibrary
 {
@@ -12,20 +13,20 @@ namespace InventoryLibrary
     {
         /// <summary> Dictionary containing all objects. </summary>
         /// <value> Comes from storage stored through JSON. </value>
-        public Dictionary<TKey,TValue> objects = new Dictionary<TKey, TValue>();
+        public Dictionary<string, object> objects = new Dictionary<string, object>();
 
         /// <summary> Used to get all objects. </summary>
         /// <returns> The entire `objects` dictionary. </returns>
-        public Dictionary<TKey,TValue> All()
+        public Dictionary<string, object> All()
         {
-            return objects;
+            return this.objects;
         }
 
         /// <summary> Adds a new key-value pair to `objects`. </summary>
         /// <param name="obj"> The new object to save into `objects`. </param>
-        public void New(Type obj)
+        public void New(BaseClass obj)
         {
-            objects.Add(GetClassName(obj) + "." + obj.id, obj);
+            this.objects.Add(obj.GetType() + "." + obj.id, obj);
         }
 
         /// <summary> Saves `objects` to the JSON file in Storage after serializing. </summary>
@@ -33,17 +34,15 @@ namespace InventoryLibrary
         {
             string JSONString = JsonSerializer.Serialize(objects);
             string JSONFile = @"../storage/inventory_manager.json";
-            using (StreamWriter sw = File.OpenWrite(JSONFile)) {
-                sw.WriteLine(JSONString);
-            }
+            File.WriteAllText(JSONFile, JSONString);
         }
 
         /// <summary> Creates `objects` based off the deserialization of the JSON file. </summary>
         public void Load()
         {
             string JSONFile = @"../storage/inventory_manager.json";
-            string JSONString = JSONFile.ReadAllText(JSONFile);
-            objects = JsonSerializer.Deserialize<Dictionary<TKey,TValue>>(JSONString);
+            string JSONString = File.ReadAllText(JSONFile);
+            this.objects = JsonSerializer.Deserialize<Dictionary<string, object>>(JSONString);
         }
     }
 }
