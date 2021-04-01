159abf402769b085909546ece34eec30e1b1506f
Michelle Giraldo <michellegsld@gmail.com>
2021-03-31 21:51:21 -0400

    [Update] Change from { get; set; }, use other way to initialize properties

--- 0x0D-csharp-text_based_interface/InventoryLibrary/Item.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/Item.cs
@@ -1,24 +1,32 @@
 ﻿using System;
+using Math;
 
 namespace InventoryLibrary
 {
-    /// <summary> A public class that sets information regarding an item. </summary>
+    /// <summary> A public class that sets information regarding an Item. </summary>
     public class Item : BaseClass
     {
-        /// <summary> The name of an item. </summary>
-        /// <value> A string. Required. </value>
-        public string name { get; set; }
+        /// <summary> A required string that's the name of an Item. </summary>
+        public string name;
 
-        /// <summary> A description of the item. </summary>
-        /// <value> A string. Optional. </value>
-        public string description { get; set; }
+        /// <summary> An optional string that's the description of the Item. </summary>
+        public string description;
 
-        /// <summary> The price of the item. </summary>
-        /// <value> A float limited to 2 decimal points. Optional. </value>
-        public float price { get; set; }
+        /// <summary> An optional 2 point float that's the price of the Item. </summary>
+        public float price;
 
-        /// <summary> The tags of the item. </summary>
-        /// <value> A list of strings. Optional. </value>
-        public List<string> tags { get; set; }
+        /// <summary> An optional list of strings that's the tags of the Item. </summary>
+        public List<string> tags = new List<string>();
+
+        public Item(string n = "Item_Name", string describe, float cost, List<string> info)
+        {
+            this.name = n;
+            if (describe)
+                this.description = describe;
+            if (cost)
+                this.price = Math.Round(cost, 2);
+            if (info)
+                this.tags = info;
+        }
     }
 }
