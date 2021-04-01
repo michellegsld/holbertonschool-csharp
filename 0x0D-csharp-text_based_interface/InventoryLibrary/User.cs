2bee4c5496704ef52516f1809249ec35bcdf9cf3
Michelle Giraldo <michellegsld@gmail.com>
2021-03-31 22:04:56 -0400

    [Update] Correct method in which property was set

--- 0x0D-csharp-text_based_interface/InventoryLibrary/User.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/User.cs
@@ -5,8 +5,12 @@ namespace InventoryLibrary
     /// <summary> A public class that sets information regarding the User. </summary>
     public class User : BaseClass
     {
-        /// <summary> The name of a User. </summary>
-        /// <value> A string. Required. </value>
-        public string name { get; set; }
+        /// <summary> A requred string that is the name of a User. </summary>
+        public string name;
+
+        public User(string n = "User_Name")
+        {
+            this.name = n;
+        }
     }
 }
