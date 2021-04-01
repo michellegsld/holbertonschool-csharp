7a55152bd866f810087a6edd5bc2df2cefb7a528
Michelle Giraldo <michellegsld@gmail.com>
2021-04-01 15:59:22 -0400

    [Update] Generate id and actually set DateTime objects

--- 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -6,12 +6,13 @@ namespace InventoryLibrary
     public class BaseClass
     {
         /// <summary> A string of the object's id. </summary>
-        public string id;
+        public string id = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
 
         /// <summary> DateTime object. The time an object was created. </summary>
-        public DateTime date_created;
+        public DateTime date_created = new DateTime();
 
         /// <summary> DateTime object. The time an object was updated. </summary>
-        public DateTime date_updated;
+        public DateTime date_updated = new DateTime();
+
     }
 }
