604e8d765e46c15de04614d4e5a3c7a5682f16b4
Michelle Giraldo <michellegsld@gmail.com>
2021-04-01 23:57:15 -0400

    [Fix] Add { get; set; } again

--- 0x0D-csharp-text_based_interface/InventoryLibrary/User.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/User.cs
@@ -6,13 +6,20 @@ namespace InventoryLibrary
     public class User : BaseClass
     {
         /// <summary> A required string that is the name of a User. </summary>
-        public string name;
+        public string name { get; set; }
 
         /// <summary> This is to initialize the User object </summary>
         /// <param name="n"> The name of the User </param>
         public User(string n)
         {
             this.name = n;
+        }
+
+        /// <summary> Organizes information about the User object. </summary>
+        /// <returns> A string with information about this object. </returns>
+        public override string ToString()
+        {
+            return base.ToString() + ": " + name.ToString();
         }
     }
 }
