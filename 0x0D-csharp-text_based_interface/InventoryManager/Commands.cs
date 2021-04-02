e3c42fddff9a8d8558d266730a5693789e96018c
Michelle Giraldo <michellegsld@gmail.com>
2021-04-01 22:21:55 -0400

    [Update] Currently add each command, continue to work on Create

--- 0x0D-csharp-text_based_interface/InventoryManager/Commands.cs
+++ 0x0D-csharp-text_based_interface/InventoryManager/Commands.cs
@@ -6,8 +6,28 @@ using InventoryLibrary;
 /// <summary> This is where each command is handled. </summary>
 public class Commands
 {
-    List<string> classnames = new List<string>() { "Base", "User", "Item", "Inventory" };
+    /// <summary> Contains all valid ClassNames </summary>
+    List<string> classnames = new List<string>() { "BaseClass", "Item", "User", "Inventory" };
 
+    /// <summary> Displays all class names of objects loaded in JSONStorage. </summary>
+    /// <param name="storage"> The JSONStorage object that contains all information. </param>
+    public void ClassNames(JSONStorage storage)
+    {
+        List<string> classes = new List<string>();
+
+        foreach(KeyValuePair<string, object> item in storage.objects) {
+            classes.Add(item.Value.GetType().Name);
+        }
+
+        HashSet<string> hash = new HashSet<string>(classes);
+        string[] found = hash.ToArray();
+
+        Console.WriteLine("\r\nResults:\r\n--------");
+        Console.WriteLine(string.Join(",", found));
+
+        if (found.Count() == 0)
+            Prompt.ErrorClass(null, 3);
+    }
 
     /// <summary> Prints all objects or all objects of a specified ClassName. </summary>
     /// <param name="storage"> The JSONStorage object that contains all information. </param>
@@ -19,10 +39,9 @@ public class Commands
 
         if (ClassName == null || classnames.Contains(ClassName, StringComparer.OrdinalIgnoreCase)) {
             foreach(KeyValuePair<string, object> item in storage.objects) {
-                string[] key = item.Key.Split(".");
                 if (ClassName == null) {
                     objs.Add(item.Value);
-                } else if (key[0].Equals(ClassName, StringComparison.OrdinalIgnoreCase)) {
+                } else if (item.Value.GetType().Name.Equals(ClassName, StringComparison.OrdinalIgnoreCase)) {
                     objs.Add(item.Value);
                 }
             }
@@ -36,11 +55,96 @@ public class Commands
         }
 
         Console.WriteLine("\r\nResults:\r\n--------");
-        Console.WriteLine(String.Join("\n", objs));
+        Console.WriteLine(String.Join("\n", objs.ToString()));
+
         if (flag >= 0)
             Prompt.ErrorClass(ClassName, flag);
+
         Console.WriteLine("\r\nThat was everything.");
-        //if (ClassName.Count() > 2)
-        //    Console.WriteLine("Any argument after the first two was ignored.\r\n");
+    }
+
+    /// <summary> Creates a new object to add to storage. </summary>
+    /// <param name="storage"> The JSONStorage object that contains all information. </param>
+    /// <param name="ClassName"> The type of object to be created. </param>
+    public void Create(JSONStorage storage, string ClassName)
+    {
+        if (classnames.Contains(ClassName, StringComparer.OrdinalIgnoreCase) == false) {
+            Prompt.ErrorClass(ClassName, 2);
+            return;
+        } else if (String.Compare(ClassName, classnames[1], StringComparison.OrdinalIgnoreCase) == 0) {
+            Console.WriteLine("\r\nPlease input the name of the Item:");
+            string name = Console.ReadLine();
+
+            if (name == "") {
+                Console.WriteLine("\r\nThe Item name is required and cannot be left blank.");
+                Console.WriteLine("\r\n...Cancelling creation of Item.\r\nCreation was cancelled.");
+                return;
+            }
+
+            Console.WriteLine("Please input a description:");
+            string describe = Console.ReadLine();
+            if (describe == "")
+                Console.WriteLine("\r\nThe Item description is optional and will be left blank.\r\n");
+
+            Console.WriteLine("Please input the price:");
+            string price = Console.ReadLine();
+            if (price == "")
+                Console.WriteLine("\r\nThe Item price is optional and will be left as 0.00\r\n");
+
+            Console.WriteLine("Please input any tags in one line and separated by spaces:");
+            string tags = Console.ReadLine();
+            if (tags == "")
+                Console.WriteLine("\r\nThe Item tags are optional and will be left blank.\r\n");
+
+            Console.WriteLine("\r\n...CREATING ITEM...");
+
+            if (price == "")
+                price = "0.00";
+
+            Item obj = new Item(name, describe, float.Parse(price), tags.Split().ToList());
+
+            storage.New(obj);
+            storage.Save();
+
+            Console.WriteLine("...SAVING ITEM...");
+            Console.WriteLine("Item successfully created!");
+        } else if (String.Compare(ClassName, classnames[2], StringComparison.OrdinalIgnoreCase) == 0) {
+            // ask for name, and other info
+            Console.WriteLine("This would make a User.");
+        } else if (String.Compare(ClassName, classnames[3], StringComparison.OrdinalIgnoreCase) == 0) {
+            // ask for name, and other info
+            Console.WriteLine("This would make an inventory");
+        }
+    }
+
+    /// <summary> Shows information about a given object from storage. </summary>
+    /// <param name="storage"> The JSONStorage object that contains all information. </param>
+    public void Show(JSONStorage storage)
+    {
+
+    }
+
+    /// <summary> Updates a given object from storage. </summary>
+    /// <param name="storage"> The JSONStorage object that contains all information. </param>
+    public void Update(JSONStorage storage)
+    {
+
+    }
+
+    /// <summary> Deletes a given object from storage. </summary>
+    /// <param name="storage"> The JSONStorage object that contains all information. </param>
+    public void Delete(JSONStorage storage)
+    {
+
+    }
+
+    /// <summary> Closes the Console Application. </summary>
+    /// <param name="storage"> The JSONStorage object that contains all information. </param>
+    public void Exit(JSONStorage storage)
+    {
+        storage.Save();
+        Console.WriteLine("\r\n...SAVING...");
+        Console.WriteLine("Goodbye!");
+        System.Environment.Exit(0);
     }
 }
