852fee9d45a0d01f0cb9fe8df608e79cc376bae9
Michelle Giraldo <michellegsld@gmail.com>
2021-04-01 22:22:34 -0400

    [Update] Add else if for each command

--- 0x0D-csharp-text_based_interface/InventoryManager/InventoryManager.cs
+++ 0x0D-csharp-text_based_interface/InventoryManager/InventoryManager.cs
@@ -23,22 +23,37 @@ namespace InventoryManager
                 input = Console.ReadLine();
                 inputSplit = input.Split();
 
-                if (commands.Contains(inputSplit[0], StringComparer.OrdinalIgnoreCase) == false) {
-                    Console.WriteLine("Please input a valid command.");
-                    continue;
+                if (inputSplit[0].Equals("ClassNames", StringComparison.OrdinalIgnoreCase)) {
+                    command.ClassNames(storage);
                 } else if (inputSplit[0].Equals("All", StringComparison.OrdinalIgnoreCase)) {
                     if (inputSplit.Count() > 1) {
                         command.All(storage, inputSplit[1]);
                     } else {
                         command.All(storage);
                     }
+                    if (inputSplit.Count() > 2)
+                        Console.WriteLine("Any argument after the first two was ignored.");
+                } else if (inputSplit[0].Equals("Create", StringComparison.OrdinalIgnoreCase)) {
+                    if (inputSplit.Count() > 1) {
+                        command.Create(storage, inputSplit[1]);
+                    } else {
+                        Console.WriteLine("Please input a ClassName.");
+                    }
+                } else if (inputSplit[0].Equals("Show", StringComparison.OrdinalIgnoreCase)) {
+                    
+                } else if (inputSplit[0].Equals("Update", StringComparison.OrdinalIgnoreCase)) {
+                    
+                } else if (inputSplit[0].Equals("Delete", StringComparison.OrdinalIgnoreCase)) {
+                    
                 } else if (inputSplit[0].Equals("Exit", StringComparison.OrdinalIgnoreCase)) {
-                    storage.Save();
-                    System.Environment.Exit(0);
+                    command.Exit(storage);
+                } else {
+                    Console.WriteLine("\r\nPlease input a valid command.\r\n");
+                    continue;
                 }
 
                 System.Threading.Thread.Sleep(1000);
-                Console.WriteLine("_________________________");
+                Console.WriteLine("\r\n_________________________");
 
                 Prompt.Print();
             }
