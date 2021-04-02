632c0e3645ebffe2deadd706ebdb299ba638aad4
Michelle Giraldo <michellegsld@gmail.com>
2021-04-01 23:57:08 -0400

    [Fix] Add { get; set; } again

--- 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace InventoryLibrary
 {
@@ -6,13 +7,19 @@ namespace InventoryLibrary
     public class BaseClass
     {
         /// <summary> A string of the object's id. </summary>
-        public string id = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Trim('=');
+        public string id { get; set; }
 
         /// <summary> DateTime object. The time an object was created. </summary>
-        public DateTime date_created = new DateTime();
+        public DateTime date_created { get; set; }
 
         /// <summary> DateTime object. The time an object was updated. </summary>
-        public DateTime date_updated = new DateTime();
+        public DateTime date_updated { get; set; }
 
+        /// <summary> Sets the basic info for each class </summary>
+        public BaseClass() {
+            this.id = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Trim('=');
+            this.date_created = DateTime.Now;
+            this.date_updated = DateTime.Now;
+        }
     }
 }
